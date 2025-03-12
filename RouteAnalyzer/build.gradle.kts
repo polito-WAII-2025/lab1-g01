b03750dc8bd49646e804409390b36d9eef21d4e4
LUIGI NICOLA MOGANO s329638 <s329638@studenti.polito.it>
2025-03-12 11:20:36 +0100

    feat read of .csv

--- RouteAnalyzer/build.gradle.kts
+++ RouteAnalyzer/build.gradle.kts
@@ -13,6 +13,7 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("org.apache.commons:commons-csv:1.10.0")
 }
 
 tasks.test {
--- RouteAnalyzer/src/main/kotlin/Main.kt
+++ RouteAnalyzer/src/main/kotlin/Main.kt
@@ -1,5 +1,22 @@
 package it.polito.wa2.g01
 
+import org.apache.commons.csv.CSVFormat
+import org.apache.commons.csv.CSVParser
+import java.io.FileReader
+
 fun main() {
-    println("Hello World!!!")
+
+    val reader = FileReader("RouteAnalyzer/src/main/resources/waypoints.csv")
+    val csvParser = CSVParser(reader, CSVFormat.DEFAULT.withDelimiter(';'))
+
+    // Itera attraverso le righe del CSV
+    for (csvRecord in csvParser) {
+        val timeStamp = csvRecord.get(0)
+        val longitudine = csvRecord.get(1)
+        val latitudine = csvRecord.get(2)
+
+        println("timeStamp: $timeStamp, Longitudine: $longitudine, Latitudine: $latitudine")
+    }
+
+    csvParser.close()
 }
