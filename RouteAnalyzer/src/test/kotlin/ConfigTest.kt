d19da4b6bde50152daa0284e46aeb053ccc7d572
LUIGI NICOLA MOGANO s329638 <s329638@studenti.polito.it>
2025-03-26 11:44:43 +0100

    fix: fixed ConfigLoader, fixed ConfigTest, added custom-parameters invalid, malformed and missing fields yml

---
+++ RouteAnalyzer/src/test/kotlin/ConfigTest.kt
@@ -0,0 +1,60 @@
+package it.polito.wa2.g01
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.nio.file.Paths
+
+class ConfigTest {
+
+    @Test
+    fun `should load valid configuration from yaml`() {
+
+        val filePath = Paths.get("../evaluation/custom-parameters.yml").toString()
+        val config = ConfigLoader.load(filePath)
+        assertEquals(6371.0, config.earthRadiusKm)
+        assertEquals(45.4642, config.geofenceCenterLatitude)
+        assertEquals(9.1900, config.geofenceCenterLongitude)
+        assertEquals(5.0, config.geofenceRadiusKm)
+        assertEquals(1.5, config.mostFrequentedAreaRadiusKm)
+    }
+
+    @Test
+    fun `should throw exception if file does not exist`() {
+        val filePath = "../evaluation/custom-param.yml"
+        val exception = assertThrows<IllegalArgumentException> {
+            ConfigLoader.load(filePath)
+        }
+        assertTrue(exception.message!!.contains("Configuration file not found"))
+    }
+
+    @Test
+    fun `should throw exception on malformed yaml`() {
+        val filePath = Paths.get("src/test/resources/custom-parameters-malformed.yml").toString()
+        val exception = assertThrows<IllegalStateException> {
+            ConfigLoader.load(filePath)
+        }
+        assertTrue(exception.message!!.contains("Error loading configuration"))
+    }
+
+    @Test
+    fun `should throw exception on invalid values`() {
+        val filePath = Paths.get("src/test/resources/custom-parameters-invalid-values.yml").toString()
+        val exception = assertThrows<IllegalStateException> {
+            ConfigLoader.load(filePath)
+        }
+        assertTrue(exception.cause is IllegalArgumentException)
+        assertTrue(exception.cause!!.message!!.contains("must be greater than 0"))
+    }
+
+    @Test
+    fun `should throw exception if required fields are missing in yaml`() {
+        val filePath = Paths.get("src/test/resources/custom-parameters-missing-required-fields.yml").toString()
+
+        val exception = assertThrows<IllegalStateException> {
+            ConfigLoader.load(filePath)
+        }
+        assertTrue(exception.cause is IllegalArgumentException)
+        assertTrue(exception.cause!!.message!!.contains("geofenceCenterLatitude is required and missing or zero"))
+    }
+}
